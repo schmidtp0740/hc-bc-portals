3afbe0e67c9470d6d8e1685440822834354c25ab
chkelley <kelleyc718@gmail.com>
2018-10-26 11:04:14 -0500

    added module css and removed un-needed components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Main from './components/Main/main';
+import Landing from './components/Misc/Landing';
 import Doctor from './components/Doctor/doctor';
 import Insurance from './components/Insurance/insurance';
 import Pharmacist from './components/Pharmacist/pharmacist';
@@ -9,16 +9,14 @@ import Pharmacist from './components/Pharmacist/pharmacist';
 export default class App extends Component {
   render() {
     return (
-      <div>
         <Router>
-          <div>      
-            <Route exact path="/" component={Main} />
+          <div>
+            <Route exact path="/" component={Landing} />
             <Route path="/doctor" component={Doctor} />
             <Route path="/pharmacist" component={Pharmacist} />
             <Route path="/insurance" component={Insurance} />
           </div>
         </Router>
-      </div>
       );
   }
 }
